d89b2322bf73cb0522227ad7c1a9bf754954b49c
macong <macong0419email@163.com>
2024-07-14 10:59:34 +0800

    实体类

---
+++ spzx-model/src/main/java/com/mac/spzx/model/entity/order/OrderStatistics.java
@@ -0,0 +1,16 @@
+package com.mac.spzx.model.entity.order;
+
+import com.mac.spzx.model.entity.base.BaseEntity;
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Data
+public class OrderStatistics extends BaseEntity {
+
+    private Date orderDate;
+    private BigDecimal totalAmount;
+    private Integer totalNum;
+    
+}
